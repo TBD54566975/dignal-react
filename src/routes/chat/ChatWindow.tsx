655511cc2afaa1778fa7a4b10eb935533d0074fa
Kirah Sapong <kirah@tbd.email>
2023-09-16 17:05:31 -0700

    Chat window styling changes

--- src/routes/chat/ChatWindow.tsx
+++ src/routes/chat/ChatWindow.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from 'react';
 import Fox from '../../assets/sample-pictures/fox.png';
 
 function ChatWindow() {
@@ -14,126 +14,160 @@ function ChatWindow() {
 
   return (
     <>
-    <div className="container text-center">
-  <div className="row-px">
-    <div className="avatar">
-      <img src={currentChat.who.picture} alt="" />
-    </div>
-    <h1>{currentChat.who.name}</h1>
-  </div>
-  <div className="history-window">
-    <div className="chat-window visually-hide-scrollbar">
-      {currentChat.messages.map((chat, index) => {
-        return (
-        <p
-          onDoubleClick={toggleThreadView}
-          onTouchStart={toggleThreadView}
-          id={index === currentChat.messages.length - 1 ? 'latest' : ''}
-          className={`${chat.from === 'self' ? "sent" : ""} ${chat.from === 'friend' ? "received" : ""}`}
-        >
-          {chat.message}
-        </p>
-        )
-      })
-    }
-    </div>
-    {inThread && <div className="thread-window visually-hide-scrollbar">
-        {currentChat.messages.map((chat, index) => {
-          return (
-          <p
-            onDoubleClick={toggleThreadView}
-            onTouchStart={toggleThreadView}
-            id={index === currentChat.messages.length - 1 ? 'latest' : ''}
-            className={`${chat.from === 'self' ? "sent" : ""} ${chat.from === 'friend' ? "received" : ""}`}
-          >
-            {chat.message}
-          </p>
-          )
-        })
-        }
-      </div>}
-  </div>
-  <div className="row-px">
-    <input type="text" className="chat-input" placeholder="Enter message" />
-  </div>
-</div>
+      <div className="container text-center">
+        <div className="row-px chat-header">
+          <div className="avatar">
+            <img src={currentChat.who.picture} alt="" />
+          </div>
+          <div className="chat-name">
+            <h2>{currentChat.who.name}</h2>
+          </div>
+        </div>
+        <div className="history-window visually-hide-scrollbar">
+          <div className="chat-window">
+            {currentChat.messages.map((chat, index) => {
+              return (
+                <p
+                  key={index}
+                  onDoubleClick={toggleThreadView}
+                  onTouchStart={toggleThreadView}
+                  id={index === currentChat.messages.length - 1 ? 'latest' : ''}
+                  className={`${chat.from === 'self' ? 'sent' : ''} ${
+                    chat.from === 'friend' ? 'received' : ''
+                  }`}
+                >
+                  {chat.message}
+                </p>
+              );
+            })}
+          </div>
+          {inThread && (
+            <div className="thread-window visually-hide-scrollbar">
+              {currentChat.messages.map((chat, index) => {
+                return (
+                  <p
+                    key={index}
+                    onDoubleClick={toggleThreadView}
+                    onTouchStart={toggleThreadView}
+                    id={
+                      index === currentChat.messages.length - 1 ? 'latest' : ''
+                    }
+                    className={`${chat.from === 'self' ? 'sent' : ''} ${
+                      chat.from === 'friend' ? 'received' : ''
+                    }`}
+                  >
+                    {chat.message}
+                  </p>
+                );
+              })}
+            </div>
+          )}
+        </div>
+        <div className="row-px message-input">
+          <input
+            type="text"
+            className="chat-input"
+            placeholder="Enter message"
+          />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default ChatWindow;
 
 const currentChat = {
   who: {
-      name: "Dignal Welcome Chat",
-      picture: Fox
+    name: 'Dignal Welcome Chat',
+    picture: Fox,
   },
-  messages: [{
-      message: "First message inbound",
-      timestamp: "2023-08-09T01:13:54.402Z",
-      from: "friend",
+  messages: [
+    {
+      message: 'First message inbound',
+      timestamp: '2023-08-09T01:13:54.402Z',
+      from: 'friend',
       delivered: true,
-      seen: true
-  }, {
-      message: "Next message outbound that goes onto a second line",
-      timestamp: "2023-08-09T01:14:54.402Z",
-      from: "self",
+      seen: true,
+    },
+    {
+      message: 'Next message outbound that goes onto a second line',
+      timestamp: '2023-08-09T01:14:54.402Z',
+      from: 'self',
       delivered: true,
-      seen: true
-  }, {
-      message: "Next message inbound that also goes onto a second line",
-      timestamp: "2023-08-09T01:15:54.402Z",
-      from: "friend",
+      seen: true,
+    },
+    {
+      message: 'Next message inbound that also goes onto a second line',
+      timestamp: '2023-08-09T01:15:54.402Z',
+      from: 'friend',
       delivered: true,
-      seen: true
-  }, {
-      message: "Last message outbound",
-      timestamp: "2023-08-11T05:33:54.402Z",
-      from: "self",
+      seen: true,
+    },
+    {
+      message: 'Last message outbound',
+      timestamp: '2023-08-11T05:33:54.402Z',
+      from: 'self',
       delivered: true,
-      seen: true
-  }, {
-      message: "Last message outbound",
-      timestamp: "2023-08-11T05:34:54.402Z",
-      from: "self",
+      seen: true,
+    },
+    {
+      message: 'Last message outbound',
+      timestamp: '2023-08-11T05:34:54.402Z',
+      from: 'self',
       delivered: true,
-      seen: true
-  }, {
-      message: "Last message outbound",
-      timestamp: "2023-08-11T05:35:54.402Z",
-      from: "self",
+      seen: true,
+    },
+    {
+      message: 'Last message outbound',
+      timestamp: '2023-08-11T05:35:54.402Z',
+      from: 'self',
       delivered: true,
-      seen: true
-  }, {
-      message: "Last message outbound",
-      timestamp: "2023-08-11T05:36:54.402Z",
-      from: "self",
+      seen: true,
+    },
+    {
+      message: 'Last message outbound',
+      timestamp: '2023-08-11T05:36:54.402Z',
+      from: 'self',
       delivered: true,
-      seen: true
-  }, {
-      message: "Last message outbound",
-      timestamp: "2023-08-11T05:37:54.402Z",
-      from: "self",
+      seen: true,
+    },
+    {
+      message: 'Last message outbound',
+      timestamp: '2023-08-11T05:37:54.402Z',
+      from: 'self',
       delivered: true,
-      seen: true
-  }, {
-      message: "Last message outbound",
-      timestamp: "2023-08-11T05:38:54.402Z",
-      from: "self",
+      seen: true,
+    },
+    {
+      message: 'Last message outbound',
+      timestamp: '2023-08-11T05:38:54.402Z',
+      from: 'self',
       delivered: true,
-      seen: true
-  }, {
-      message: "Last message outbound",
-      timestamp: "2023-08-11T05:39:54.402Z",
-      from: "self",
+      seen: true,
+    },
+    {
+      message: 'Last message outbound',
+      timestamp: '2023-08-11T05:39:54.402Z',
+      from: 'self',
       delivered: true,
-      seen: true
-  }, {
-      message: "Last message inbound",
-      timestamp: "2023-08-11T05:40:54.402Z",
-      from: "friend",
+      seen: true,
+    },
+    {
+      message: 'Last message inbound',
+      timestamp: '2023-08-11T05:40:54.402Z',
+      from: 'friend',
       delivered: true,
-      seen: true
-  }],
-  id: "123" //recordId of the thread
-}
+      seen: true,
+    },
+    {
+      message: 'Last message inbound',
+      timestamp: '2023-08-11T05:41:54.402Z',
+      from: 'friend',
+      delivered: true,
+      seen: true,
+    },
+    {},
+  ],
+  id: '123', //recordId of the thread
+};
--- src/routes/chat/Sidebar.tsx
+++ src/routes/chat/Sidebar.tsx
@@ -1,129 +1,136 @@
-import { useState } from "react";
-import { RoutePaths } from "../../routes";
-import { ProfileProtocol } from "../../util/protocols/profile.protocol";
-import { queryRecords, readRecord } from "../../util/web5";
+import { useState } from 'react';
+import { RoutePaths } from '../../routes';
+import { ProfileProtocol } from '../../util/protocols/profile.protocol';
+import { queryRecords, readRecord } from '../../util/web5';
 import Bat from '../../assets/sample-pictures/bat.png';
 import Elephant from '../../assets/sample-pictures/elephant.png';
 import Fox from '../../assets/sample-pictures/fox.png';
 
 function Sidebar() {
-  const [profilePicture, setProfilePicture] = useState("");
-  const [profileName, setProfileName] = useState("");
+  const [profilePicture, setProfilePicture] = useState('');
+  const [profileName, setProfileName] = useState('');
 
-    async function getProfile() {
-        const { records } = await queryRecords({
-            message: {
-                filter: {
-                    protocol: ProfileProtocol.protocol,
-                    schema: ProfileProtocol.types.profile.schema,
-                    dataFormat: "application/json",
-                },
-            },
-        });
+  async function getProfile() {
+    const { records } = await queryRecords({
+      message: {
+        filter: {
+          protocol: ProfileProtocol.protocol,
+          schema: ProfileProtocol.types.profile.schema,
+          dataFormat: 'application/json',
+        },
+      },
+    });
 
-        if (records) {
-            const profiledata = await records[records.length - 1].data.json();
+    if (records) {
+      const profiledata = await records[records.length - 1].data.json();
 
-            const { record: photodata } = await readRecord({
-                message: {
-                    recordId: profiledata.picture,
-                },
-            });
-            const blob = await photodata.data.blob();
-            setProfileName(profiledata.name);
-            setProfilePicture(URL.createObjectURL(blob));
-        }
+      const { record: photodata } = await readRecord({
+        message: {
+          recordId: profiledata.picture,
+        },
+      });
+      const blob = await photodata.data.blob();
+      setProfileName(profiledata.name);
+      setProfilePicture(URL.createObjectURL(blob));
     }
+  }
 
-    getProfile();
-
-
+  getProfile();
 
   return (
-<div>
-    <div className="profile-row">
+    <div>
+      <div className="profile-row">
         <div className="avatar">
-            <img src={profilePicture} alt="" />
+          <img src={profilePicture} alt="" />
         </div>
         <h1>{profileName}</h1>
-    </div>
-    <ul className="messages">
+      </div>
+      <ul className="messages">
         {chats.map((chat, index) => {
           return (
             <li key={index}>
-                <a href={RoutePaths.CHAT + "/" + chat.id} className="message-row">
-                    <div className={`notification ${!chat.what.seen ? "notification-active" : ""}`}>
-                        <span className="sr-only" aria-live="assertive">{chat.what.seen ? "Read": "Unread"}</span>
-                    </div>
-                    <div className="avatar">
-                        <img src={chat.who.picture} alt="" />
-                    </div>
-                    <div className="contents">
-                        <div className="message">
-                            <h2>{chat.who.name}</h2>
-                            <p>{chat.what.message}</p>
-                        </div>
-                        <div className="meta">
-                            <p className={!chat.what.seen ? "text-highlight" : ""}>{chat.what.timestamp}</p>
-                            {chat.what.from === "self" && chat.what.seen && <p className="text-xxs">Seen</p>}
-                        </div>
-                    </div>
-                </a>
+              <a href={RoutePaths.CHAT + '/' + chat.id} className="message-row">
+                <div
+                  className={`notification ${
+                    !chat.what.seen ? 'notification-active' : ''
+                  }`}
+                >
+                  <span className="sr-only" aria-live="assertive">
+                    {chat.what.seen ? 'Read' : 'Unread'}
+                  </span>
+                </div>
+                <div className="avatar">
+                  <img src={chat.who.picture} alt="" />
+                </div>
+                <div className="contents">
+                  <div className="message">
+                    <h2>{chat.who.name}</h2>
+                    <p>{chat.what.message}</p>
+                  </div>
+                  <div className="meta">
+                    <p className={!chat.what.seen ? 'text-highlight' : ''}>
+                      {chat.what.timestamp}
+                    </p>
+                    {chat.what.from === 'self' && chat.what.seen && (
+                      <p className="text-xxs">Seen</p>
+                    )}
+                  </div>
+                </div>
+              </a>
             </li>
-          )
+          );
         })}
-            
-    </ul>
-</div>
-  )
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
 
 const chats = [
   {
-      who: {
-          name: "Dignal Welcome Chat",
-          picture: Fox
-      },
-      what: {
-          message: "Last message outbound",
-          timestamp: "2023-08-09T01:13:54.402Z",
-          from: "self",
-          delivered: true,
-          seen: true
-      },
-      id: "123" //recordId of the thread
+    who: {
+      name: 'Dignal Welcome Chat',
+      picture: Fox,
+    },
+    what: {
+      message: 'Last message outbound',
+      timestamp: '2023-08-09T01:13:54.402Z',
+      from: 'self',
+      delivered: true,
+      seen: true,
+    },
+    id: '123', //recordId of the thread
   },
   {
-      who: {
-          name: "Rando friend",
-          picture: Elephant
-      },
-      what: {
-          message: "Last message inbound continues onto second line",
-          timestamp: "2023-08-07T01:13:54.402Z",
-          from: "friend",
-          delivered: true,
-          seen: true
-      },
-      id: "456"
+    who: {
+      name: 'Rando friend',
+      picture: Elephant,
+    },
+    what: {
+      message: 'Last message inbound continues onto second line',
+      timestamp: '2023-08-07T01:13:54.402Z',
+      from: 'friend',
+      delivered: true,
+      seen: true,
+    },
+    id: '456',
   },
   {
-      who: {
-          name: "Other rando friend",
-          picture: Bat
-      },
-      what: {
-          message: "Last message inbound continues onto second line and is unread",
-          timestamp: "2023-08-07T01:13:54.402Z",
-          from: "friend",
-          delivered: true,
-          seen: false
-      },
-      id: "789"
-  }
-]
+    who: {
+      name: 'Other rando friend',
+      picture: Bat,
+    },
+    what: {
+      message: 'Last message inbound continues onto second line and is unread',
+      timestamp: '2023-08-07T01:13:54.402Z',
+      from: 'friend',
+      delivered: true,
+      seen: false,
+    },
+    id: '789',
+  },
+];
 
 for (const chat of chats) {
   chat.what.timestamp = convertTime(chat.what.timestamp);
@@ -137,16 +144,28 @@ function convertTime(time: string) {
   const oneWeek = 7 * oneDay;
 
   if (timeDifference < oneDay && inputTime.getDate() === now.getDate()) {
-      const formattedTime = `${inputTime.getHours()}:${String(inputTime.getMinutes()).padStart(2, '0')}`;
-      const period = inputTime.getHours() >= 12 ? 'pm' : 'am';
-      return `${formattedTime} ${period}`;
+    const formattedTime = `${inputTime.getHours()}:${String(
+      inputTime.getMinutes(),
+    ).padStart(2, '0')}`;
+    const period = inputTime.getHours() >= 12 ? 'pm' : 'am';
+    return `${formattedTime} ${period}`;
   } else if (timeDifference < 2 * oneDay) {
-      return 'Yesterday';
+    return 'Yesterday';
   } else if (timeDifference < oneWeek) {
-      const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-      return daysOfWeek[inputTime.getDay()];
+    const daysOfWeek = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+    return daysOfWeek[inputTime.getDay()];
   } else {
-      const formattedDate = `${inputTime.getFullYear()}-${String(inputTime.getMonth() + 1).padStart(2, '0')}-${String(inputTime.getDate()).padStart(2, '0')}`;
-      return formattedDate;
+    const formattedDate = `${inputTime.getFullYear()}-${String(
+      inputTime.getMonth() + 1,
+    ).padStart(2, '0')}-${String(inputTime.getDate()).padStart(2, '0')}`;
+    return formattedDate;
   }
 }
