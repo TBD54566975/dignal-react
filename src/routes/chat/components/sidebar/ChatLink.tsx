493deb2fc16c26c4eb930460ced1e7ecc955dca0
Kirah Sapong <kirah@tbd.email>
2023-10-03 15:11:56 -0700

    Update ChatLink delete button conditional render

--- src/routes/chat/components/sidebar/ChatLink.tsx
+++ src/routes/chat/components/sidebar/ChatLink.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { NavLink, useMatch, useNavigate } from 'react-router-dom';
 import { RoutePaths } from '@/routes';
 import { IChatRecord } from '@routes/chat/types';
 import Close from '@assets/icons/x-close.svg';
@@ -7,8 +7,9 @@ import { deleteMessagesFromDwn, hideSidebar } from '../../utils';
 import { queryFromDwnMessageReplies } from '../../dwn';
 
 function ChatLink({ chat }: { chat: IChatRecord }) {
+  const navRoute = RoutePaths.CHAT + '/' + chat.record.id;
   const navigate = useNavigate();
-  const location = useLocation();
+  const match = useMatch(navRoute);
 
   async function deleteChat(e: MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
@@ -16,21 +17,15 @@ function ChatLink({ chat }: { chat: IChatRecord }) {
     // so other participant can reignite chat context if needed
     const records = await queryFromDwnMessageReplies(chat.record.contextId);
     if (records && records.length > 0) {
-      await deleteMessagesFromDwn(records);
-    }
-    if (location.pathname.includes(chat.record.contextId)) {
-      navigate(RoutePaths.CHAT);
+      deleteMessagesFromDwn(records).then(() => {
+        navigate(RoutePaths.CHAT, { replace: true });
+      });
     }
   }
 
   return (
     <>
-      <NavLink
-        to={RoutePaths.CHAT + '/' + chat.record.id}
-        onClick={hideSidebar}
-        end
-        className="message-row"
-      >
+      <NavLink to={navRoute} onClick={hideSidebar} end className="message-row">
         <div className="avatar-container">
           <div className="avatar">
             <img src={chat.picture} alt="" />
@@ -42,9 +37,11 @@ function ChatLink({ chat }: { chat: IChatRecord }) {
           </div>
         </div>
       </NavLink>
-      <button className="icon-button highlight-button" onClick={deleteChat}>
-        <img width="16" src={Close} alt="" /> Delete
-      </button>
+      {match && (
+        <button className="icon-button highlight-button" onClick={deleteChat}>
+          <img width="16" src={Close} alt="" /> Delete
+        </button>
+      )}
     </>
   );
 }
